991e6e066de17ba1b0af7fe25fd64f1dd6d7b9a1
Nikita <bybalesh234@gmail.com>
2024-04-14 15:45:30 +0600

    Initial commit

---
+++ tests-examples/demo-todo-app.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+test("Counter Test", async ({ page }) => {
+    await page.goto('https://www.avito.ru/avito-care/eco-impact');
+    
+    const ValueElement = await page.$('//div[contains(@class, "desktop-value-Nd1tR")]');
+    
+    if (ValueElement) {
+        const ValueText = await ValueElement.textContent();
+        const actualValue = parseInt(ValueText.trim());
+        
+        expect(actualValue).not.toBeNaN(); // Проверка, что значение является числом
+
+        // Примерные проверки на размер числа, в том числе равенство 0
+        if (actualValue > 0) {
+            expect(actualValue).toBeGreaterThan(0); // Положительное число
+        } else if (actualValue < 0) {
+            expect(actualValue).toBeLessThan(0); // Отрицательное число
+        } else {
+            expect(actualValue).toBe(0); // Значение равно 0
+        }
+    } else {
+        throw new Error('Элемент CO2 не найден на странице.');
+    }
+});
