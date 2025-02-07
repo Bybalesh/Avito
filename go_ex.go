7dd286156f7e54353a883a98fc03227e319dd6f0
Nik <bybalesh234@gmail.com>
2025-02-07 11:24:27 +0600

    Update go_ex.go

--- go_ex.go
+++ go_ex.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+ "fmt"
+ "math"
+)
+
+// Константы
+const earthRadius float64 = 6371.0 // km
+const omskLatitude float64 = 54.95  // degrees
+
+// Функция для перевода градусов в радианы
+func degreesToRadians(degrees float64) float64 {
+ return degrees * math.Pi / 180.0
+}
+
+// Функция для вычисления расстояния от экватора
+func distanceFromEquator(latitude float64) float64 {
+ latitudeRadians := degreesToRadians(latitude)
+ return earthRadius * latitudeRadians
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+ distance := distanceFromEquator(omskLatitude)
+ fmt.Printf("Расстояние от экватора до Омска: %.0f км\n", math.Round(distance))
 }
